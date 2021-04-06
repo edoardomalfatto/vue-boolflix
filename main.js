4fff0446afc8c2138bffcdec3a499334c5c79690
Edoardo <edoardo.malfatto@hotmail.it>
2021-04-06 14:31:23 +0200

    agg

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ Vue.config.devtools = true;
 var root = new Vue({
     el: '#app',
     data: {
+        countrycode: "en",
         searchedFilm: "",
-        searchResult: []
+        searchResult: [],
+        aviableLanguage: []
     },
     methods: {
         searchFilm: function() {
@@ -12,7 +14,12 @@ var root = new Vue({
             axios.get('https://api.themoviedb.org/3/search/movie?api_key=df59ca4f93077e59f74858e7b9ae9776&page=1&include_adult=false&language=IT', { params: { query: this.searchedFilm } })
                 .then((response) => {
                     response.data.results.forEach(film => {
+                        film.vote_average = Math.ceil(parseInt(film.vote_average / 2));
                         this.searchResult.push(film);
+                        if (!this.aviableLanguage.includes(film.original_language)) {
+                            this.aviableLanguage.push(film.original_language);
+                            console.log(this.aviableLanguage);
+                        }
                     });
                 })
         }
