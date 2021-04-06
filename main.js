81083c9d868ac7b80d001865125e1d4ebae4f49f
Edoardo <edoardo.malfatto@hotmail.it>
2021-04-06 12:25:27 +0200

    agg

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ var root = new Vue({
     },
     methods: {
         searchFilm: function() {
+            this.searchResult = [];
             axios.get('https://api.themoviedb.org/3/search/movie?api_key=df59ca4f93077e59f74858e7b9ae9776&page=1&include_adult=false&language=IT', { params: { query: this.searchedFilm } })
                 .then((response) => {
                     response.data.results.forEach(film => {
