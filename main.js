f808fd7341df1f6659fbf7e465e0b46804d8f191
Edoardo <edoardo.malfatto@hotmail.it>
2021-04-06 12:12:14 +0200

    milestone 1

---
+++ main.js
@@ -0,0 +1,20 @@
+Vue.config.devtools = true;
+
+var root = new Vue({
+    el: '#app',
+    data: {
+        searchedFilm: "",
+        searchResult: []
+    },
+    methods: {
+        searchFilm: function() {
+            axios.get('https://api.themoviedb.org/3/search/movie?api_key=df59ca4f93077e59f74858e7b9ae9776&page=1&include_adult=false&language=IT', { params: { query: this.searchedFilm } })
+                .then((response) => {
+                    response.data.results.forEach(film => {
+                        this.searchResult.push(film);
+                    });
+                    this.searchedFilm = ""
+                })
+        }
+    }
+});
