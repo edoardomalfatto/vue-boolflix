2e800bd9a67c7055a98fd688e12408e85b19f8fe
Edoardo <edoardo.malfatto@hotmail.it>
2021-04-07 15:01:01 +0200

    agg milestone 2

--- main.js
+++ main.js
@@ -6,7 +6,13 @@ var root = new Vue({
         countrycode: "en",
         searchedFilm: "",
         searchResult: [],
-        aviableLanguage: []
+        flagUrls: {
+            it: "https://www.countryflags.io/it/flat/64.png",
+            fr: "https://www.countryflags.io/fr/flat/64.png",
+            en: "https://www.countryflags.io/us/flat/64.png",
+            es: "https://www.countryflags.io/es/flat/64.png",
+            de: "https://www.countryflags.io/de/flat/64.png"
+        }
     },
     methods: {
         searchFilm: function() {
@@ -16,12 +22,25 @@ var root = new Vue({
                     response.data.results.forEach(film => {
                         film.vote_average = Math.ceil(parseInt(film.vote_average / 2));
                         this.searchResult.push(film);
-                        if (!this.aviableLanguage.includes(film.original_language)) {
-                            this.aviableLanguage.push(film.original_language);
-                            console.log(this.aviableLanguage);
-                        }
                     });
                 })
+            axios.get('https://api.themoviedb.org/3/search/tv?api_key=df59ca4f93077e59f74858e7b9ae9776&page=1&include_adult=false&language=IT', { params: { query: this.searchedFilm } })
+                .then((response) => {
+                    response.data.results.forEach(tvserie => {
+                        tvserie.vote_average = Math.ceil(parseInt(tvserie.vote_average / 2));
+                        this.searchResult.push(tvserie);
+                        console.log(this.searchResult)
+                    });
+                })
+        },
+        getFlag(language) {
+            return this.flagUrls[language];
+        },
+        getPosterImage(image) {
+            return ("https://image.tmdb.org/t/p/w342/" + image);
         }
     }
 });
+
+
+//se film.original language non è incluso nell'array di bandiere che ho allora metti una stringa predefinita
