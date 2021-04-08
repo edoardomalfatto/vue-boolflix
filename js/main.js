ec50b1851565846a796fe7c8410262076a8f1197
Edoardo <edoardo.malfatto@hotmail.it>
2021-04-08 14:50:40 +0200

    main

--- js/main.js
+++ js/main.js
@@ -17,14 +17,14 @@ var root = new Vue({
     methods: {
         searchFilm: function() {
             this.searchResult = [];
-            axios.get('https://api.themoviedb.org/3/search/movie?api_key=df59ca4f93077e59f74858e7b9ae9776&page=1&include_adult=false&language=IT', { params: { query: this.searchedFilm } })
+            axios.get('https://api.themoviedb.org/3/search/movie?api_key=df59ca4f93077e59f74858e7b9ae9776&page=1&include_adult=false', { params: { query: this.searchedFilm } })
                 .then((response) => {
                     response.data.results.forEach(film => {
                         film.vote_average = Math.ceil(parseInt(film.vote_average / 2));
                         this.searchResult.push(film);
                     });
                 })
-            axios.get('https://api.themoviedb.org/3/search/tv?api_key=df59ca4f93077e59f74858e7b9ae9776&page=1&include_adult=false&language=IT', { params: { query: this.searchedFilm } })
+            axios.get('https://api.themoviedb.org/3/search/tv?api_key=df59ca4f93077e59f74858e7b9ae9776&page=1&include_adult=false', { params: { query: this.searchedFilm } })
                 .then((response) => {
                     response.data.results.forEach(tvserie => {
                         tvserie.vote_average = Math.ceil(parseInt(tvserie.vote_average / 2));
